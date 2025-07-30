80d5610aaa2280e85b8afa2264efaf615ee7a182
navot <navotj@gmail.com>
2025-07-30 10:36:21 +0300

    eof indentation

--- main.tf
+++ main.tf
@@ -62,8 +62,8 @@ resource "aws_instance" "mongodb" {
     #!/bin/bash
     apt-get update
     apt-get install gnupg curl
-    curl -fsSL https://pgp.mongodb.com/server-7.0.asc | \
-    gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+    curl -fsSL https://pgp.mongodb.com/server-8.0.asc | \
+    gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg \
     --dearmor
     echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
     apt-get update
@@ -72,7 +72,7 @@ resource "aws_instance" "mongodb" {
     systemctl enable mongodb
     sleep 10
     mongo admin --eval 'db.createUser({user:"${var.mongodb_user}",pwd:"${var.mongodb_password}",roles:[{role:"root",db:"admin"}]})'
-    EOF
+EOF
   tags = {
     Name = "terraform-mongodb"
   }
