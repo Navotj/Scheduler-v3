be47d7e3721b47148a3c5cdb7d8a31bf12c54ac1
navot <navotj@gmail.com>
2025-07-31 10:35:31 +0300

    added more envs

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -1,6 +1,11 @@
 output "backend_instance_ip" {
-  description = "Public IP of the backend EC2 instance (Node.js/Python app)"
+  description = "Public IP of the backend EC2 instance"
   value       = aws_instance.backend.public_ip
+}
+
+output "backend_instance_private_ip" {
+  description = "Private IP of the backend EC2 instance"
+  value       = aws_instance.backend.private_ip
 }
 
 output "backend_instance_id" {
@@ -13,7 +18,17 @@ output "mongodb_instance_ip" {
   value       = aws_instance.mongodb.public_ip
 }
 
+output "mongodb_instance_private_ip" {
+  description = "Private IP of the MongoDB EC2 instance"
+  value       = aws_instance.mongodb.private_ip
+}
+
 output "mongodb_instance_id" {
   description = "Instance ID of the MongoDB EC2 instance"
   value       = aws_instance.mongodb.id
+}
+
+output "mongodb_ebs_volume_id" {
+  description = "EBS volume ID attached to the MongoDB instance (if used)"
+  value       = aws_ebs_volume.mongodb_storage.id
 }
