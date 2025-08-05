8ffe3322d522f02de977426cd46d9db910a72174
navot <navotj@gmail.com>
2025-08-05 11:52:12 +0300

    checking ssm

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -121,25 +121,54 @@ resource "aws_security_group" "backend_access" {
 }
 
 
+######################
+# IAM for SSM Access #
+######################
+
+resource "aws_iam_role" "ec2_ssm_role" {
+  name = "EC2SSMRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_core" {
+  role       = aws_iam_role.ec2_ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ec2_ssm_instance_profile" {
+  name = "EC2SSMInstanceProfile"
+  role = aws_iam_role.ec2_ssm_role.name
+}
+
 
 #################
 # EC2 Instances #
 #################
 
-# MongoDB Instance
 resource "aws_instance" "mongodb" {
-  ami                         = "ami-0c1b03e30bca3b373"
-  instance_type               = "t3.micro"
-  subnet_id                   = data.aws_subnet.eu_central_1b.id
-  availability_zone           = "eu-central-1b"
-  vpc_security_group_ids      = [aws_security_group.mongodb_access.id]
-  key_name = "terraform-ec2"
+  ami                    = "ami-0c1b03e30bca3b373"
+  instance_type          = "t3.micro"
+  subnet_id              = data.aws_subnet.eu_central_1b.id
+  availability_zone      = "eu-central-1b"
+  vpc_security_group_ids = [aws_security_group.mongodb_access.id]
+  key_name               = "terraform-ec2"
+  iam_instance_profile   = aws_iam_instance_profile.ec2_ssm_instance_profile.name
 
   user_data = templatefile("${path.module}/mongo_install.sh.tmpl", {
-    admin_mongo_user        = var.admin_mongo_user
-    admin_mongo_password    = var.admin_mongo_password
-    s3_mongo_user           = var.s3_mongo_user
-    s3_mongo_password       = var.s3_mongo_password
+    admin_mongo_user     = var.admin_mongo_user
+    admin_mongo_password = var.admin_mongo_password
+    s3_mongo_user        = var.s3_mongo_user
+    s3_mongo_password    = var.s3_mongo_password
   })
 
   tags = {
@@ -147,17 +176,16 @@ resource "aws_instance" "mongodb" {
   }
 }
 
-# Python/Node.js Instance
 resource "aws_instance" "backend" {
-  ami                         = "ami-0c1b03e30bca3b373"
-  instance_type               = "t3.micro"
-  subnet_id                   = data.aws_subnet.eu_central_1b.id
-  availability_zone           = "eu-central-1b"
-  vpc_security_group_ids      = [aws_security_group.backend_access.id]
-  key_name = "terraform-ec2"
+  ami                    = "ami-0c1b03e30bca3b373"
+  instance_type          = "t3.micro"
+  subnet_id              = data.aws_subnet.eu_central_1b.id
+  availability_zone      = "eu-central-1b"
+  vpc_security_group_ids = [aws_security_group.backend_access.id]
+  key_name               = "terraform-ec2"
+  iam_instance_profile   = aws_iam_instance_profile.ec2_ssm_instance_profile.name
 
   user_data = templatefile("${path.module}/backend_install.sh.tmpl", {})
-
 
   tags = {
     Name = "terraform-backend"
