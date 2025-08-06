2fc73a17121cbb6f4ae47ada255fd5ccc3b30147
navot <navotj@gmail.com>
2025-08-06 12:57:15 +0300

    fixed nameservers?

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -290,6 +290,27 @@ resource "aws_route53_zone" "main" {
   name = "nat20scheduling.com"
 }
 
+resource "aws_route53domains_registered_domain" "main" {
+  domain_name = "nat20scheduling.com"
+
+  name_server {
+    name = aws_route53_zone.main.name_servers[0]
+  }
+
+  name_server {
+    name = aws_route53_zone.main.name_servers[1]
+  }
+
+  name_server {
+    name = aws_route53_zone.main.name_servers[2]
+  }
+
+  name_server {
+    name = aws_route53_zone.main.name_servers[3]
+  }
+}
+
+
 resource "aws_route53_record" "mongo" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "mongo.nat20scheduling.com"
