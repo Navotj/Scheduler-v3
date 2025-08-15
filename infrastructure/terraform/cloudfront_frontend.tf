966bcab52729013cb63c5c5d01326e547b4aed36
Navotj <navotj@gmail.com>
2025-08-16 02:59:33 +0300

    removing duplicates

--- infrastructure/terraform/cloudfront_frontend.tf
+++ infrastructure/terraform/cloudfront_frontend.tf
@@ -58,46 +58,6 @@ resource "aws_cloudfront_distribution" "frontend" {
 }
 
 ############################################################
-# ACM Certificate for CloudFront
-############################################################
-
-resource "aws_acm_certificate" "frontend" {
-  provider          = aws.us_east_1
-  domain_name       = "nat20scheduling.com"
-  validation_method = "DNS"
-
-  subject_alternative_names = [
-    "www.nat20scheduling.com"
-  ]
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_route53_record" "frontend_cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.frontend.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      type   = dvo.resource_record_type
-      record = dvo.resource_record_value
-    }
-  }
-
-  zone_id = data.aws_route53_zone.main.zone_id
-  name    = each.value.name
-  type    = each.value.type
-  records = [each.value.record]
-  ttl     = 60
-}
-
-resource "aws_acm_certificate_validation" "frontend" {
-  provider                = aws.us_east_1
-  certificate_arn         = aws_acm_certificate.frontend.arn
-  validation_record_fqdns = [for record in aws_route53_record.frontend_cert_validation : record.fqdn]
-}
-
-############################################################
 # DNS Record for CloudFront
 ############################################################
 
