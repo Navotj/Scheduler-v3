c827feab6024fe6b9ee47763538e6412453390dd
Navotj <navotj@gmail.com>
2025-08-16 02:52:26 +0300

    asdasdsadsa

--- infrastructure/terraform/cloudfront_frontend.tf
+++ infrastructure/terraform/cloudfront_frontend.tf
@@ -1,45 +1,35 @@
-provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"
-}
-
-data "aws_route53_zone" "main" {
-  name = var.domain_name
-}
-
-resource "aws_cloudfront_origin_access_control" "frontend" {
-  name                              = "nat20-frontend-oac"
-  description                       = "Origin Access Control for Frontend"
-  origin_access_control_origin_type = "s3"
-  signing_behavior                  = "always"
-  signing_protocol                  = "sigv4"
-}
+############################################################
+# CloudFront Frontend Distribution
+############################################################
 
 resource "aws_cloudfront_distribution" "frontend" {
   provider = aws.us_east_1
 
+  origin {
+    domain_name = aws_s3_bucket.frontend.bucket_regional_domain_name
+    origin_id   = "frontend-s3-origin"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.frontend.cloudfront_access_identity_path
+    }
+  }
+
   enabled             = true
   is_ipv6_enabled     = true
-  comment             = "Frontend Distribution"
   default_root_object = "index.html"
 
-  aliases = [var.domain_name]
-
-  origin {
-    domain_name = aws_s3_bucket.deploy_artifacts.bucket_regional_domain_name
-    origin_id   = "s3-frontend"
-
-    origin_access_control_id = aws_cloudfront_origin_access_control.frontend.id
-  }
+  aliases = [
+    "nat20scheduling.com",
+    "www.nat20scheduling.com"
+  ]
 
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "s3-frontend"
+    target_origin_id = "frontend-s3-origin"
 
     forwarded_values {
       query_string = false
-
       cookies {
         forward = "none"
       }
@@ -51,6 +41,8 @@ resource "aws_cloudfront_distribution" "frontend" {
     max_ttl                = 86400
   }
 
+  price_class = "PriceClass_100"
+
   restrictions {
     geo_restriction {
       restriction_type = "none"
@@ -58,17 +50,77 @@ resource "aws_cloudfront_distribution" "frontend" {
   }
 
   viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate.cloudfront.arn
+    acm_certificate_arn      = aws_acm_certificate.frontend.arn
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.2_2021"
   }
 
-  web_acl_id = var.attach_frontend_waf ? aws_wafv2_web_acl.frontend.arn : null
+  web_acl_id = aws_wafv2_web_acl.frontend.arn
+
+  tags = {
+    Name = "nat20-frontend-cf"
+  }
 }
 
-resource "aws_route53_record" "frontend" {
+resource "aws_cloudfront_origin_access_identity" "frontend" {
+  comment = "OAI for nat20 frontend CloudFront"
+}
+
+resource "aws_acm_certificate" "frontend" {
+  provider = aws.us_east_1
+  domain_name       = "nat20scheduling.com"
+  validation_method = "DNS"
+
+  subject_alternative_names = [
+    "www.nat20scheduling.com"
+  ]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = "nat20-frontend-cert"
+  }
+}
+
+resource "aws_route53_record" "frontend_cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.frontend.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
   zone_id = data.aws_route53_zone.main.zone_id
-  name    = var.domain_name
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "frontend" {
+  provider                = aws.us_east_1
+  certificate_arn         = aws_acm_certificate.frontend.arn
+  validation_record_fqdns = [for record in aws_route53_record.frontend_cert_validation : record.fqdn]
+}
+
+resource "aws_route53_record" "frontend_alias" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "nat20scheduling.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.frontend.domain_name
+    zone_id                = aws_cloudfront_distribution.frontend.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "frontend_alias_www" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "www.nat20scheduling.com"
   type    = "A"
 
   alias {
