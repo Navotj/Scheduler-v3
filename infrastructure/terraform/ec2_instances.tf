de7b50442d126265e790e1cf141a15db12d784b5
Navotj <navotj@gmail.com>
2025-08-16 11:17:32 +0300

    aaaaa

--- infrastructure/terraform/ec2_instances.tf
+++ infrastructure/terraform/ec2_instances.tf
@@ -8,8 +8,6 @@ resource "aws_instance" "mongodb" {
   subnet_id              = data.aws_subnet.eu_central_1b.id
   availability_zone      = "eu-central-1b"
   vpc_security_group_ids = [aws_security_group.mongodb_access.id]
-  key_name               = "terraform-ec2"
-  iam_instance_profile   = aws_iam_instance_profile.ssm_ec2_profile.name
 
   # No secrets here; script pulls creds from SSM at runtime
   user_data = templatefile("${path.module}/mongo_install.sh.tmpl", {})
@@ -25,8 +23,6 @@ resource "aws_instance" "backend" {
   subnet_id              = data.aws_subnet.eu_central_1b.id
   availability_zone      = "eu-central-1b"
   vpc_security_group_ids = [aws_security_group.backend_access.id]
-  key_name               = "terraform-ec2"
-  iam_instance_profile   = aws_iam_instance_profile.ssm_ec2_profile.name
 
   user_data = templatefile("${path.module}/backend_install.sh.tmpl", {})
 
