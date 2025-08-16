381bfa58cc69924067960a1e36bb4a509abd7c88
Navotj <navotj@gmail.com>
2025-08-16 03:11:00 +0300

    xxxxx

--- infrastructure/terraform/acm_cloudfront.tf
+++ infrastructure/terraform/acm_cloudfront.tf
@@ -1,16 +1,10 @@
-provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"
-}
-
-data "aws_route53_zone" "main" {
-  name         = "nat20scheduling.com."
-  private_zone = false
-}
+############################################################
+# ACM Certificate for CloudFront
+############################################################
 
 resource "aws_acm_certificate" "frontend" {
   provider          = aws.us_east_1
-  domain_name       = "nat20scheduling.com"
+  domain_name       = var.frontend_domain
   validation_method = "DNS"
 
   lifecycle {
@@ -22,17 +16,16 @@ resource "aws_route53_record" "frontend_cert_validation" {
   for_each = {
     for dvo in aws_acm_certificate.frontend.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
-      type   = dvo.resource_record_type
       record = dvo.resource_record_value
+      type   = dvo.resource_record_type
     }
   }
 
-  allow_overwrite = true
-  name            = each.value.name
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.main.zone_id
-  records         = [each.value.record]
-  ttl             = 60
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.record]
 }
 
 resource "aws_acm_certificate_validation" "frontend" {
