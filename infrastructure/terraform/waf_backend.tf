fa07bea512fa964fabbb66c155ffedb7c66e591f
Navotj <navotj@gmail.com>
2025-08-18 21:46:36 +0300

    tukiytrfd

--- infrastructure/terraform/waf_backend.tf
+++ infrastructure/terraform/waf_backend.tf
@@ -7,20 +7,34 @@ resource "aws_wafv2_web_acl" "backend" {
   scope       = "REGIONAL"
   description = "Backend WAF: managed rule groups + healthcheck/OPTIONS allow + rate limit + Mongo-safe guards"
 
-  default_action { allow {} }
+  default_action {
+    allow {}
+  }
 
   # 0 - Allow ALB health checks (User-Agent contains ELB-HealthChecker)
   rule {
     name     = "AllowALBHealthChecks"
     priority = 0
-    action { allow {} }
+
+    action {
+      allow {}
+    }
 
     statement {
       byte_match_statement {
         search_string         = "ELB-HealthChecker"
         positional_constraint = "CONTAINS"
-        field_to_match { single_header { name = "user-agent" } }
-        text_transformation { priority = 0, type = "NONE" }
+
+        field_to_match {
+          single_header {
+            name = "user-agent"
+          }
+        }
+
+        text_transformation {
+          priority = 0
+          type     = "NONE"
+        }
       }
     }
 
@@ -35,14 +49,24 @@ resource "aws_wafv2_web_acl" "backend" {
   rule {
     name     = "AllowCORSPreflight"
     priority = 1
-    action { allow {} }
+
+    action {
+      allow {}
+    }
 
     statement {
       byte_match_statement {
         search_string         = "OPTIONS"
         positional_constraint = "EXACTLY"
-        field_to_match { method {} }
-        text_transformation { priority = 0, type = "NONE" }
+
+        field_to_match {
+          method {}
+        }
+
+        text_transformation {
+          priority = 0
+          type     = "NONE"
+        }
       }
     }
 
@@ -57,7 +81,10 @@ resource "aws_wafv2_web_acl" "backend" {
   rule {
     name     = "RateLimitPerIP"
     priority = 5
-    action { block {} }
+
+    action {
+      block {}
+    }
 
     statement {
       rate_based_statement {
@@ -77,22 +104,40 @@ resource "aws_wafv2_web_acl" "backend" {
   rule {
     name     = "BlockMongoOperatorsInInputs"
     priority = 6
-    action { block {} }
+
+    action {
+      block {}
+    }
 
     statement {
       or_statement {
         statement {
           regex_match_statement {
             regex_string = "(?i)\\$(ne|gt|gte|lt|lte|in|nin|or|and|where)\\b"
-            field_to_match { query_string {} }
-            text_transformation { priority = 0, type = "NONE" }
+
+            field_to_match {
+              query_string {}
+            }
+
+            text_transformation {
+              priority = 0
+              type     = "NONE"
+            }
           }
         }
+
         statement {
           regex_match_statement {
             regex_string = "(?i)\\$(ne|gt|gte|lt|lte|in|nin|or|and|where)\\b"
-            field_to_match { body {} }
-            text_transformation { priority = 0, type = "NONE" }
+
+            field_to_match {
+              body {}
+            }
+
+            text_transformation {
+              priority = 0
+              type     = "NONE"
+            }
           }
         }
       }
@@ -109,47 +154,84 @@ resource "aws_wafv2_web_acl" "backend" {
   rule {
     name     = "RequireJsonOnWriteMethods"
     priority = 7
-    action { block {} }
+
+    action {
+      block {}
+    }
 
     statement {
       and_statement {
+        # Part A: Method is POST or PUT or PATCH
         statement {
           or_statement {
             statement {
               byte_match_statement {
                 search_string         = "POST"
                 positional_constraint = "EXACTLY"
-                field_to_match { method {} }
-                text_transformation { priority = 0, type = "NONE" }
+
+                field_to_match {
+                  method {}
+                }
+
+                text_transformation {
+                  priority = 0
+                  type     = "NONE"
+                }
               }
             }
+
             statement {
               byte_match_statement {
                 search_string         = "PUT"
                 positional_constraint = "EXACTLY"
-                field_to_match { method {} }
-                text_transformation { priority = 0, type = "NONE" }
+
+                field_to_match {
+                  method {}
+                }
+
+                text_transformation {
+                  priority = 0
+                  type     = "NONE"
+                }
               }
             }
+
             statement {
               byte_match_statement {
                 search_string         = "PATCH"
                 positional_constraint = "EXACTLY"
-                field_to_match { method {} }
-                text_transformation { priority = 0, type = "NONE" }
+
+                field_to_match {
+                  method {}
+                }
+
+                text_transformation {
+                  priority = 0
+                  type     = "NONE"
+                }
               }
             }
           }
         }
 
+        # Part B: NOT (Content-Type contains application/json)
         statement {
           not_statement {
             statement {
               byte_match_statement {
                 search_string         = "application/json"
                 positional_constraint = "CONTAINS"
-                field_to_match { single_header { name = "content-type" } }
-                text_transformation { priority = 0, type = "NONE" }
+
+                field_to_match {
+                  single_header {
+                    name = "content-type"
+                  }
+                }
+
+                text_transformation {
+                  priority = 0
+                  type     = "NONE"
+                }
               }
             }
           }
@@ -168,21 +250,32 @@ resource "aws_wafv2_web_acl" "backend" {
   rule {
     name     = "LimitJsonBodySize"
     priority = 8
-    action { block {} }
+
+    action {
+      block {}
+    }
 
     statement {
       byte_match_statement {
         search_string         = "{"
         positional_constraint = "STARTS_WITH"
+
         field_to_match {
           json_body {
             match_scope               = "ALL"
             invalid_fallback_behavior = "MATCH"
             oversize_handling         = "MATCH"
-            match_pattern { all {} }
+
+            match_pattern {
+              all {}
+            }
           }
         }
-        text_transformation { priority = 0, type = "NONE" }
+
+        text_transformation {
+          priority = 0
+          type     = "NONE"
+        }
       }
     }
 
@@ -193,37 +286,96 @@ resource "aws_wafv2_web_acl" "backend" {
     }
   }
 
-  # Managed rules
+  # 10 - Core protections
   rule {
     name     = "AWS-AWSManagedRulesCommonRuleSet"
     priority = 10
-    override_action { none {} }
-    statement { managed_rule_group_statement { name = "AWSManagedRulesCommonRuleSet", vendor_name = "AWS" } }
-    visibility_config { cloudwatch_metrics_enabled = true, metric_name = "CommonRuleSet", sampled_requests_enabled = true }
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "CommonRuleSet"
+      sampled_requests_enabled   = true
+    }
   }
 
+  # 20 - Known bad inputs
   rule {
     name     = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
     priority = 20
-    override_action { none {} }
-    statement { managed_rule_group_statement { name = "AWSManagedRulesKnownBadInputsRuleSet", vendor_name = "AWS" } }
-    visibility_config { cloudwatch_metrics_enabled = true, metric_name = "KnownBadInputs", sampled_requests_enabled = true }
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesKnownBadInputsRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "KnownBadInputs"
+      sampled_requests_enabled   = true
+    }
   }
 
+  # 30 - IP reputation
   rule {
     name     = "AWS-AWSManagedRulesAmazonIpReputationList"
     priority = 30
-    override_action { none {} }
-    statement { managed_rule_group_statement { name = "AWSManagedRulesAmazonIpReputationList", vendor_name = "AWS" } }
-    visibility_config { cloudwatch_metrics_enabled = true, metric_name = "IpReputation", sampled_requests_enabled = true }
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesAmazonIpReputationList"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "IpReputation"
+      sampled_requests_enabled   = true
+    }
   }
 
+  # 40 - SQL injection detection
   rule {
     name     = "AWS-AWSManagedRulesSQLiRuleSet"
     priority = 40
-    override_action { none {} }
-    statement { managed_rule_group_statement { name = "AWSManagedRulesSQLiRuleSet", vendor_name = "AWS" } }
-    visibility_config { cloudwatch_metrics_enabled = true, metric_name = "SQLi", sampled_requests_enabled = true }
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesSQLiRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "SQLi"
+      sampled_requests_enabled   = true
+    }
   }
 
   visibility_config {
--- infrastructure/terraform/waf_frontend.tf
+++ infrastructure/terraform/waf_frontend.tf
@@ -8,20 +8,35 @@ resource "aws_wafv2_web_acl" "frontend" {
   scope       = "CLOUDFRONT"
   description = "Frontend WAF: managed rule groups + OPTIONS allow + rate limit"
 
-  default_action { allow {} }
+  default_action {
+    allow {}
+  }
 
+  # 0 - Always allow CORS preflight requests
   rule {
     name     = "AllowCORSPreflight"
     priority = 0
-    action { allow {} }
+
+    action {
+      allow {}
+    }
+
     statement {
       byte_match_statement {
         search_string         = "OPTIONS"
         positional_constraint = "EXACTLY"
-        field_to_match { method {} }
-        text_transformation { priority = 0, type = "NONE" }
+
+        field_to_match {
+          method {}
+        }
+
+        text_transformation {
+          priority = 0
+          type     = "NONE"
+        }
       }
     }
+
     visibility_config {
       cloudwatch_metrics_enabled = true
       metric_name                = "AllowCORSPreflight"
@@ -29,16 +44,22 @@ resource "aws_wafv2_web_acl" "frontend" {
     }
   }
 
+  # 5 - IP rate limiting (safe baseline)
   rule {
     name     = "RateLimitPerIP"
     priority = 5
-    action { block {} }
+
+    action {
+      block {}
+    }
+
     statement {
       rate_based_statement {
         limit              = 1500
         aggregate_key_type = "IP"
       }
     }
+
     visibility_config {
       cloudwatch_metrics_enabled = true
       metric_name                = "RateLimitPerIP"
@@ -46,36 +67,96 @@ resource "aws_wafv2_web_acl" "frontend" {
     }
   }
 
+  # 10 - Core protections (low FP)
   rule {
     name     = "AWS-AWSManagedRulesCommonRuleSet"
     priority = 10
-    override_action { none {} }
-    statement { managed_rule_group_statement { name = "AWSManagedRulesCommonRuleSet", vendor_name = "AWS" } }
-    visibility_config { cloudwatch_metrics_enabled = true, metric_name = "CommonRuleSet", sampled_requests_enabled = true }
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "CommonRuleSet"
+      sampled_requests_enabled   = true
+    }
   }
 
+  # 20 - Known bad inputs
   rule {
     name     = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
     priority = 20
-    override_action { none {} }
-    statement { managed_rule_group_statement { name = "AWSManagedRulesKnownBadInputsRuleSet", vendor_name = "AWS" } }
-    visibility_config { cloudwatch_metrics_enabled = true, metric_name = "KnownBadInputs", sampled_requests_enabled = true }
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesKnownBadInputsRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "KnownBadInputs"
+      sampled_requests_enabled   = true
+    }
   }
 
+  # 30 - IP reputation
   rule {
     name     = "AWS-AWSManagedRulesAmazonIpReputationList"
     priority = 30
-    override_action { none {} }
-    statement { managed_rule_group_statement { name = "AWSManagedRulesAmazonIpReputationList", vendor_name = "AWS" } }
-    visibility_config { cloudwatch_metrics_enabled = true, metric_name = "IpReputation", sampled_requests_enabled = true }
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesAmazonIpReputationList"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "IpReputation"
+      sampled_requests_enabled   = true
+    }
   }
 
+  # 40 - SQL injection detection
   rule {
     name     = "AWS-AWSManagedRulesSQLiRuleSet"
     priority = 40
-    override_action { none {} }
-    statement { managed_rule_group_statement { name = "AWSManagedRulesSQLiRuleSet", vendor_name = "AWS" } }
-    visibility_config { cloudwatch_metrics_enabled = true, metric_name = "SQLi", sampled_requests_enabled = true }
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesSQLiRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "SQLi"
+      sampled_requests_enabled   = true
+    }
   }
 
   visibility_config {
@@ -86,13 +167,6 @@ resource "aws_wafv2_web_acl" "frontend" {
 }
 
 # Conditional association to CloudFront distribution
-resource "aws_cloudfront_distribution" "frontend_dependency" {
-  # This dummy resource block is not needed; distribution already exists in cloudfront.tf
-  # Association resource below will reference the existing distribution directly.
-  # Placeholder to make file self-contained for readability (no-op).
-  count = 0
-}
-
 resource "aws_wafv2_web_acl_association" "frontend_cf_assoc" {
   provider     = aws.us_east_1
   resource_arn = aws_cloudfront_distribution.frontend.arn
