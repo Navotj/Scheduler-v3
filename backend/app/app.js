6df2ded17ef4bf0f22c7dd59846d35144f65a271
navot <navotj@gmail.com>
2025-08-27 11:24:33 +0300

    5342243

--- backend/app/app.js
+++ backend/app/app.js
@@ -26,7 +26,7 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-app.options('/:path(*)', cors(corsOptions));
+app.options('/:path(.*)', cors(corsOptions));
 
 
 app.use(express.json({ limit: '1mb' }));
