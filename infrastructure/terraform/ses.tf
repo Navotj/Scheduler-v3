b28ffe297eaf6864cbbd996fa635fbf77758e740
navot <navotj@gmail.com>
2025-08-31 15:24:47 +0300

    fixing mail auth

--- infrastructure/terraform/ses.tf
+++ infrastructure/terraform/ses.tf
@@ -2,14 +2,12 @@ resource "aws_sesv2_email_identity" "domain" {
   email_identity = data.aws_route53_zone.root.name
 }
 
-# DKIM records for SES (3 CNAMEs)
+# DKIM records (3 CNAMEs). Use count (static keys) so planning works before tokens are known.
 resource "aws_route53_record" "ses_dkim" {
-  for_each = toset(aws_sesv2_email_identity.domain.dkim_signing_attributes[0].tokens)
-  zone_id  = data.aws_route53_zone.root.zone_id
-  name     = "${each.value}._domainkey.${data.aws_route53_zone.root.name}"
-  type     = "CNAME"
-  ttl      = 300
-  records  = ["${each.value}.dkim.amazonses.com"]
+  count   = 3
+  zone_id = data.aws_route53_zone.root.zone_id
+  name    = "${aws_sesv2_email_identity.domain.dkim_signing_attributes[0].tokens[count.index]}._domainkey.${data.aws_route53_zone.root.name}"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["${aws_sesv2_email_identity.domain.dkim_signing_attributes[0].tokens[count.index]}.dkim.amazonses.com"]
 }
-
-# Optional MAIL FROM domain can be added later if desired
